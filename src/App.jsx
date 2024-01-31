81db45734eafde85272560413416ec7997223058
Prog4 <prog4@amado-id.ru>
2024-01-31 15:52:34 +0500

    before form

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import { Layout } from 'antd';
+import AppLayout from './components/layout/AppLayout';
+import { CryptoContextProvider } from './context/crypto-context';
 
-const headerStyle = {
-  textAlign: 'center',
-  color: '#fff',
-  height: 60,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
-};
-const contentStyle = {
-  textAlign: 'center',
-  minHeight: 'calc(100vh - 60px)',
-  color: '#fff',
-  backgroundColor: '#001529',
-};
-const siderStyle = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#1677ff',
-};
-const layoutStyle = {
-  borderRadius: 8,
-  overflow: 'hidden',
-};
 
 export default function App() {
-  return (<Layout style={layoutStyle}>
-    <Layout>
-      <Layout.Sider width="25%" style={siderStyle}>
-        Sider
-      </Layout.Sider>
-      <Layout.Content style={contentStyle}>Content</Layout.Content>
-    </Layout>
-  </Layout>)
+  return (
+    <CryptoContextProvider>
+      <AppLayout/>
+    </CryptoContextProvider>
+  )
 }
--- src/components/Layout/AppHeader.jsx
+++ src/components/Layout/AppHeader.jsx
@@ -1,15 +1,74 @@
-import { Layout } from 'antd';
+import { Layout, Select, Space, Button, Modal, Drawer } from 'antd';
+import { useCrypto } from '../../context/crypto-context';
+import { useEffect, useState } from 'react';
+import CoinInfoModal from '../CoinInfoModal';
+import AddAssetForm from '../AddAssetForm';
 
 
 const headerStyle = {
+  with: '100%',
   textAlign: 'center',
-  color: '#fff',
   height: 60,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
+  padding: '1rem',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
 };
 
-export function AppHeader(){
-    return <Layout.Header style={headerStyle}>Header</Layout.Header>
+export default function AppHeader() {
+  const {crypto} = useCrypto()
+  const [modal, setModal] = useState(false)
+  const [select, setSelect] = useState(false)
+  const [coin, setCoin] = useState(false)
+  const [drawer, setDrawer] = useState(false)
+
+  const handleSelect = (value) => {
+    setCoin(crypto.find(coin => coin.id === value))
+    setModal(true)
+  }
+
+  useEffect(() => {
+    const keypress = event => {
+      if(event.key == '/'){
+        setSelect(prev => !prev)
+      } 
+    }
+  
+    document.addEventListener('keypress', keypress)
+
+    return () => document.removeEventListener('keypress', keypress)
+  }, [])
+
+  return <Layout.Header style={headerStyle}>
+    <Select
+      style={{
+        width: '250px',
+      }}
+      value={'press / to open'}
+      options={crypto.map(coin => ({
+        label:coin.name,
+        value: coin.id,
+        icon: coin.icon
+      }))}
+      optionRender={(option) => (
+        <Space>
+          <img style={{width:'20px'}} src={option.data.icon} alt={option.data.label}/> {option.data.label}
+          {option.data.desc}
+        </Space>
+      )}
+      onSelect={handleSelect}
+      open={select}
+      onClick={() => setSelect(prev => !prev)}
+
+    />
+    <Button type="primary" onClick={() => setDrawer(true)}>Add asset</Button>
+
+    <Modal footer={false} open={modal} onOk={() => setModal(false)} onCancel={() => setModal(false)}>
+        <CoinInfoModal coin={coin}/>
+      </Modal>
+
+      <Drawer destroyOnClose width={600} title="Add Asset" onClose={() => setDrawer(false)} open={drawer}>
+        <AddAssetForm/>
+      </Drawer>
+  </Layout.Header>
 }
